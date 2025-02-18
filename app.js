b049f35e7e1d818827b4ba532a1ed62ad18682b8
RConradGe <rcgadduang@gmail.com>
2025-02-18 08:09:24 -0800

    Displaying Numbers

--- app.js
+++ app.js
@@ -1,9 +1,53 @@
 /*-------------------------------- Constants --------------------------------*/
 
 /*-------------------------------- Variables --------------------------------*/
-
+let num1 = 0;
+let num2 = 0;
+let operator = "";
 /*------------------------ Cached Element References ------------------------*/
-
+const buttons = document.querySelectorAll('.button');
+const display = document.querySelector('.display')
+const calculator = document.querySelector('#calculator');
 /*----------------------------- Event Listeners -----------------------------*/
 
+
+
+buttons.forEach((button) => {
+  button.addEventListener('click', (event) => {
+    const buttonValue = event.target.innerText;
+
+    if (buttonValue === "C") {
+      display.innerText = ""
+      return;
+    }
+
+    display.innerText += buttonValue;
+  });
+});
+
+calculator.addEventListener('click', (event) => {
+
+
+  // Example
+  if (event.target.classList.contains('number')) {
+    num1 = buttonValue;
+    console.log(num1);
+  }
+
+  // Example
+  if (event.target.innerText === '*') {
+    // Do something with this operator
+  }
+
+  if (event.target.classList.contains('operator')) {
+
+  }
+});
+
 /*-------------------------------- Functions --------------------------------*/
+
+const add = (num1, num2, operator) => {
+    if (operator === "+" ) {
+      return num1 + num2;
+    } 
+}
