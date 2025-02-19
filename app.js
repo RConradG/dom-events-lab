3448f1712323642f6ec701e7c3e6e61b1fd12a82
RConradGe <rcgadduang@gmail.com>
2025-02-18 23:37:29 -0800

    Created function headers for operators

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 /*-------------------------------- Constants --------------------------------*/
 
 /*-------------------------------- Variables --------------------------------*/
+
 let num1 = 0;
 let num2 = 0;
 let operator = "";
+
 /*------------------------ Cached Element References ------------------------*/
+
 const buttons = document.querySelectorAll('.button');
 const display = document.querySelector('.display')
 const calculator = document.querySelector('#calculator');
+
 /*----------------------------- Event Listeners -----------------------------*/
 
 
@@ -27,27 +31,49 @@ buttons.forEach((button) => {
   });
 });
 
-calculator.addEventListener('click', (event) => {
+// calculator.addEventListener('click', (event) => {
 
 
-  // Example
-  if (event.target.classList.contains('number')) {
-    num1 = buttonValue;
-    console.log(num1);
-  }
+//   // Example
+//   if (event.target.classList.contains('number')) {
+//     num1 = buttonValue;
+//     console.log(num1);
+//   }
 
-  // Example
-  if (event.target.innerText === '*') {
-    // Do something with this operator
-  }
+//   // Example
+//   if (event.target.innerText === '*') {
+//     // Do something with this operator
+//   }
 
-  if (event.target.classList.contains('operator')) {
+//   if (event.target.classList.contains('operator')) {
 
-  }
-});
+//   }
+// });
 
 /*-------------------------------- Functions --------------------------------*/
+function handleClick() {
 
+}
+
+function add(num1, num2, operator) {
+  // add two numbers
+}
+
+function subtract(num1, num2, operator) {
+  // subtract num2 from num 1
+}
+
+function multiply(num1, num2, operator) {
+  // multiply both nummbers
+}
+
+function divide(num1, num2, operator) {
+  // make sure num2 is not 0
+    // if it is, return a message undefined
+  
+    // if num2 is not 0
+    // divide num 1 by num 2
+}
 const add = (num1, num2, operator) => {
     if (operator === "+" ) {
       return num1 + num2;
